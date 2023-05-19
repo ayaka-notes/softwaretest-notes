10e3de0f057685fcc82db3c10c256b64da3a8437
zhangziqian <84625273+Musicminion@users.noreply.github.com>
2023-05-19 09:33:10 +0000

    修复

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/Chapter01">
-            Docusaurus Tutorial - 5min ⏱️
+            开启期末复习 - 5min ⏱️
           </Link>
         </div>
       </div>
